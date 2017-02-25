079396502b00408f2d41f8ea1d2a579b841a8829
AVGaidai <diligent20494@gmail.com>
2017-02-25 20:01:26 +0700

    add files

---
+++ env.sh
@@ -0,0 +1,13 @@
+
+sudo dnf -y install gcc     
+sudo dnf -y install gcc-c++  
+sudo dnf -y install clang    
+sudo dnf -y install gdb      
+sudo dnf -y install pkgconfig      
+sudo dnf -y install valgrind 
+sudo dnf -y install binutils     #gprof
+sudo dnf -y install make     
+sudo dnf -y install doxygen  
+sudo dnf -y install vim      
+sudo dnf -y install emacs    
+
