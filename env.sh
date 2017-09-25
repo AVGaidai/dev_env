4f5451c1bcb8ea62327da67982f426ecb8d81b2c
AVGaidai <diligent20494@gmail.com>
2017-09-25 11:07:24 +0700

    add mpicc install

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@ sudo dnf -y update
 
 sudo dnf -y install gcc     
 sudo dnf -y install gcc-c++  
+sudo dnf -y install mpicc
 sudo dnf -y install clang    
 sudo dnf -y install gdb      
 sudo dnf -y install pkgconfig      
