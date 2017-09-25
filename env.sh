55cc659870b7ab8aaf46edea7814fdf1a623fa10
AVGaidai <diligent20494@gmail.com>
2017-09-25 11:50:22 +0700

    fixed errata

--- env.sh
+++ env.sh
@@ -3,7 +3,6 @@ sudo dnf -y update
 
 sudo dnf -y install gcc     
 sudo dnf -y install gcc-c++  
-sudo dnf -y install mpicc
 sudo dnf -y install clang    
 sudo dnf -y install gdb      
 sudo dnf -y install pkgconfig      
